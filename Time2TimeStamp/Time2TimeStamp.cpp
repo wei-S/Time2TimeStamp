a90759b315a271e9cde639d6f2a0952335f5f524
wei <310134924@qq.com>
2020-12-20 21:51:36 +0800

    时间戳与时间的相互转换

--- Time2TimeStamp/Time2TimeStamp.cpp
+++ Time2TimeStamp/Time2TimeStamp.cpp
@@ -7,26 +7,100 @@ Time2TimeStamp::Time2TimeStamp(QWidget *parent)
     init();
 }
 
+
 void Time2TimeStamp::init()
 {
     m_isRun = true;
 
+    //connect
+    connect(ui.pushButtonCurrentContorl,&QPushButton::clicked,this,&Time2TimeStamp::onButtonCurrentContorlClick);
+    connect(ui.pushButtonConvert1, &QPushButton::clicked, this, &Time2TimeStamp::onButtonTimeStamp2Time);
+    connect(ui.pushButtonConvert2, &QPushButton::clicked, this, &Time2TimeStamp::onButtonTime2TimeStamp);
+
+    //Timer
     m_pTimer = new QTimer;
     connect(m_pTimer, &QTimer::timeout, this, &Time2TimeStamp::onTimeOut);
     m_pTimer->start(TIME_OUT);
 
     //widget init
-    ui.dateTimeEdit1->setEnabled(false);
-    ui.lineEditTimeStamp2->setEnabled(false);
+    //ui.dateTimeEdit1->setEnabled(false);
+    //ui.lineEditTimeStamp2->setEnabled(false);
+    ui.pushButtonCurrentContorl->setText("Stop");
+    ui.labelCurrentTime->setStyleSheet("color:green;");
+
+    //TimeStamp Range
+    ui.lineEditTimeStamp1->setValidator(new QIntValidator(1, 10000000000, this));
+    ui.lineEditTimeStamp2->setValidator(new QIntValidator(1, 10000000000, this));
 
     QDateTime currTime = QDateTime::currentDateTime();
     ui.labelCurrentTime->setText(QString::number(currTime.toSecsSinceEpoch()));
     ui.lineEditTimeStamp1->setText(QString::number(currTime.toSecsSinceEpoch()));
     ui.dateTimeEdit1->setDateTime(currTime);
     ui.dateTimeEdit2->setDateTime(currTime);
+
+    //label click  event filer
+    ui.labelCurrentTime->installEventFilter(this);
 }
+
+bool Time2TimeStamp::eventFilter(QObject* obj, QEvent* event)
+{
+    if (obj == ui.labelCurrentTime)
+    {//labelCurrentTime LeftButton Event
+        if (event->type() == QEvent::MouseButtonPress)
+        {
+            QMouseEvent *pMouseEvent = static_cast<QMouseEvent*>(event);
+            if (pMouseEvent->button() == Qt::LeftButton)
+            {
+                ui.lineEditTimeStamp1->setText(ui.labelCurrentTime->text()); //copy
+                ui.dateTimeEdit2->setDateTime(QDateTime::fromSecsSinceEpoch(ui.labelCurrentTime->text().toULongLong()));
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 
 void Time2TimeStamp::onTimeOut()
 {
-    ui.labelCurrentTime->setText(QString::number(QDateTime::currentDateTime().toSecsSinceEpoch()));
+	ui.labelCurrentTime->setText(QString::number(QDateTime::currentDateTime().toSecsSinceEpoch()));
+}
+
+void Time2TimeStamp::onButtonCurrentContorlClick()
+{
+    m_isRun = !m_isRun;
+    if (m_isRun)
+    {
+        m_pTimer->start(TIME_OUT);
+        ui.pushButtonCurrentContorl->setText("Stop");
+        ui.labelCurrentTime->setStyleSheet("color:green;");
+    }
+    else
+    {
+        if (m_pTimer->isActive())
+        {
+            m_pTimer->stop();
+            ui.pushButtonCurrentContorl->setText("Start");
+            ui.labelCurrentTime->setStyleSheet("color:red;");
+        }
+    }
+}
+
+void Time2TimeStamp::onButtonTimeStamp2Time()
+{
+    qint64 timeStamp = ui.lineEditTimeStamp1->text().toULongLong();
+
+    if (timeStamp > 0)
+    {
+        ui.dateTimeEdit1->setDateTime(QDateTime::fromSecsSinceEpoch(timeStamp));
+    }
+}
+
+void Time2TimeStamp::onButtonTime2TimeStamp()
+{
+    QDateTime dt = ui.dateTimeEdit2->dateTime();
+    if (!dt.isNull())
+    {
+        ui.lineEditTimeStamp2->setText(QString::number(dt.toSecsSinceEpoch()));
+    }
 }
--- Time2TimeStamp/Time2TimeStamp.h
+++ Time2TimeStamp/Time2TimeStamp.h
@@ -4,6 +4,8 @@
 #include "ui_Time2TimeStamp.h"
 #include <QTimer>
 #include <QDateTime>
+#include <QEvent>
+#include <QMouseEvent>
 
 #define TIME_OUT 1000
 
@@ -14,11 +16,16 @@ class Time2TimeStamp : public QMainWindow
 public:
 	Time2TimeStamp(QWidget* parent = Q_NULLPTR);
 
+protected:
+	bool eventFilter(QObject* obj, QEvent* event);
 private:
 	void init();
 
 private slots:
 	void onTimeOut();
+	void onButtonCurrentContorlClick();
+	void onButtonTimeStamp2Time();
+	void onButtonTime2TimeStamp();
 
 private:
 	Ui::Time2TimeStampClass ui;
